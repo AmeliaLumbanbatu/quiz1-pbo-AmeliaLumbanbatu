db71fb81c7121caec9e30646e4b9126485580512
AmeliaLumbanbatu <renatamelia05@gmail.com>
2026-02-24 10:31:43 +0700

    kuis PBO

--- src/driver/Driver3.java
+++ src/driver/Driver3.java
@@ -1,3 +1,18 @@
+package driver;
+
+import model.JadwalLaundry;
+import java.util.Scanner;
+
 public class Driver3 {
-    
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        System.out.println("=== SISTEM LAUNDRY DEL ===");
+        System.out.print("Nama: "); String nama = sc.nextLine();
+        System.out.print("Asrama: "); String asrama = sc.nextLine();
+        System.out.print("Hari: "); String hari = sc.nextLine();
+        System.out.print("Berat (kg): "); double berat = sc.nextDouble();
+
+        JadwalLaundry laundry = new JadwalLaundry(nama, asrama, berat, hari);
+        laundry.tampilkanDetail();
+  }  
 }
